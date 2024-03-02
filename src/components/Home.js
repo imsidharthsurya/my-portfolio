ff156dfe5e352125dec35d104256e337a4f5032a
Sidharth Surya <imsidharthsurya@gmail.com>
2024-03-03 00:57:39 +0530

    portfolio btn styled

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,16 @@ import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div name="home" className='h-screen w-full bg-gradient-to-b from-black to-gray-800'>
+    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
         <div className='flex flex-col justify-center h-full'>
             <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Full Stack Developer</h2>
             <p className='text-gray-500 py-4 max-w-md'>A dynamic developer, passionate about crafting cutting-edge solutions. With a robust background in the MERN (MongoDB, Express.js, React.js, Node.js) stack, chat bots and IVR bots.</p>
         
             <div>
-                <button>Portfolio <span><MdOutlineKeyboardArrowRight/></span></button>
+                <button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500'>
+                    Portfolio <span className='group-hover:rotate-90 duration-300'><MdOutlineKeyboardArrowRight size={22}/></span>
+                </button>
             </div>
         </div>
         <div>
