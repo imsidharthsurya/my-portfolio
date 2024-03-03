48b9634ecc29b987a12f4ab887d8ba3e91efdb54
Sidharth Surya <imsidharthsurya@gmail.com>
2024-03-03 19:59:07 +0530

    all done 2 projects needs to be added

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,36 +1,54 @@
 import React from "react";
-import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
 import installNode from "../assets/portfolio/installNode.jpg";
-import navbar from "../assets/portfolio/navbar.jpg";
-import reactParallax from "../assets/portfolio/reactParallax.jpg";
-import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
 import reactWeather from "../assets/portfolio/reactWeather.jpg";
+import pic1 from "../assets/portfolio/p1.png"
+import pic2 from "../assets/portfolio/p2.png"
+import pic3 from "../assets/portfolio/p3.png"
+import pic4 from "../assets/portfolio/p4.png"
 
 const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
-      src: arrayDestruct,
+      src: pic1,
+      name:"Taaza Kitchen",
+      demoUrl:"https://sid-taaza-kitchen.netlify.app/",
+      codeUrl:"https://github.com/imsidharthsurya/food-ordering-app"
     },
     {
       id: 2,
-      src: reactParallax,
+      src: pic2,
+      name:"My Youtube",
+      demoUrl:"https://github.com/imsidharthsurya/youtube-app",
+      codeUrl:"https://github.com/imsidharthsurya/youtube-app"
     },
     {
       id: 3,
-      src: navbar,
+      src: pic3,
+      name:"Flash The Movie Bot",
+      demoUrl:"https://www.loom.com/share/780ef0e968f34ab29968624fad7bfeaa",
+      codeUrl:"https://www.loom.com/share/780ef0e968f34ab29968624fad7bfeaa"
     },
     {
       id: 4,
-      src: reactSmooth,
+      src: pic4,
+      name:"Microsoft 365 Plugin",
+      demoUrl:"https://www.site24x7.com/community/microsoft-365-subscription-license-plugin",
+      codeUrl:"https://github.com/site24x7/plugins/tree/master/Microsoft365ProductsSubscriptionLicenseCheck"
     },
     {
       id: 5,
       src: installNode,
+      name:"My Youtube",
+      demoUrl:"https://github.com/imsidharthsurya/youtube-app",
+      codeUrl:"https://github.com/imsidharthsurya/youtube-app"
     },
     {
       id: 6,
       src: reactWeather,
+      name:"My Youtube",
+      demoUrl:"https://github.com/imsidharthsurya/youtube-app",
+      codeUrl:"https://github.com/imsidharthsurya/youtube-app"
     },
   ];
 
@@ -48,18 +66,19 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src,name,demoUrl,codeUrl }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
                 alt=""
                 className="rounded-md duration-200 hover:scale-105"
               />
+              <p className="text-center text-lg font-bold mt-5">{name}</p>
               <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <button onClick={() => window.open(demoUrl, '_blank')} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   Demo
                 </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <button onClick={() => window.open(codeUrl, '_blank')} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   Code
                 </button>
               </div>
