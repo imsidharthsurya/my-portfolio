10bf6a2a66c5720afbaac0cc23ddd0cd83bfc19a
Sidharth Surya <imsidharthsurya@gmail.com>
2024-03-04 14:05:23 +0530

    final

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -4,7 +4,6 @@ import html from "../assets/html.png";
 import css from "../assets/css.png";
 import javascript from "../assets/javascript.png";
 import reactImage from "../assets/react.png";
-import graphql from "../assets/graphql.png";
 import github from "../assets/github.png";
 import tailwind from "../assets/tailwind.png";
 import nodejs from "../assets/node.png"
