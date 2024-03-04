7c79347ded86933dd8afc5390eaf4d729c02a244
Sidharth Surya <imsidharthsurya@gmail.com>
2024-03-04 14:37:11 +0530

    remove warning from SocialLink

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -9,7 +9,7 @@ const SocialLinks = () => {
         <ul>
             {
                 socialData.map((data)=>{
-                    return <li key={data.id} className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] bg-gray-500 hover:ml-[-10px] hover:rounded-md duration-300'+' '+data.style}>
+                    return <li key={data.id} className={`flex justify-between items-center w-40 h-14 px-4 ml-[-100px] bg-gray-500 hover:ml-[-10px] hover:rounded-md duration-300 ${data.style}`}>
                                 <a href={data.href} target='_blank' rel="noreferrer" className='flex justify-between items-center w-full text-white'>
                                     {data.child}
                                 </a>
